49339fa06ca9f22c417cfeb4a7b02c8154117fcb
Ankit Rout <88599131+ankitrout2903@users.noreply.github.com>
2023-06-26 13:45:47 +0530

    Update todo.js

--- todo/frontend/pages/todo.js
+++ todo/frontend/pages/todo.js
@@ -1,11 +1,41 @@
+import { useRouter } from "next/router"
+import { useEffect, useState } from "react"
+import LayoutAuthenticated from "../components/layout-authenticated"
+import styles from "../styles/styles.module.css"
 import Head from 'next/head'
 import Layout from '../components/layout';
 import ToDoList from '../components/todo-list';
 
-export default function Home() {
+export default function Todo() {
+  const [content, setContent] = useState(null)
+  const router = useRouter()
+
+  useEffect(() => {
+    fetchContent()
+  }, [])
+
+
+  async function fetchContent() {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/test/user`, {
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + localStorage.getItem("token")
+      }
+    })
+    if (res.ok) {
+      const text = await res.text()
+      setContent(text)
+    }
+  }
+
   return (
-    <div>
-      <Head>
+    <LayoutAuthenticated>
+      <div className={styles.container}>
+        <h1 className={styles.title}>User</h1>
+        {content && (
+          <p>{content}</p>
+        )}
+         <Head>
         <title>NExtJs To Do</title>
         <meta name="description" content="Full Stack Book To Do" />
         <link rel="icon" href="/favicon.ico" />
@@ -13,6 +43,10 @@ export default function Home() {
       <Layout>
         <ToDoList />
       </Layout>
-    </div>
+      </div>
+
+    </LayoutAuthenticated>
+    
+    
   )
 }
